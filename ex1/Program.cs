44340f23cba13cfc78abb3913bffda97dbd4ffd2
Alexey <kalnitskiy2018@mail.ru>
2023-07-16 15:05:54 +0500

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 10000 && n > 100000){
+    System.Console.WriteLine("Число не является пятизначным");
+}
+else{
+    string SrtN = n.ToString();
+    if(SrtN[0] == SrtN[4] && SrtN[1] == SrtN[3]){
+        System.Console.WriteLine("Это полиндром");
+    }
+    else {
+        System.Console.WriteLine("Это не полиндром");
+    }
+}
+
+
+
